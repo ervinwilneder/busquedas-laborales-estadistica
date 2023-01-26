f2f6560742978e503b22ab481d973109f5b798b6
Ervin Wilneder <ervinwilneder@gmail.com>
2023-01-26 11:45:12 -0300

    rename csv file name adding date

--- etl.py
+++ etl.py
@@ -1,6 +1,9 @@
 import json
 import pandas as pd
 import re
+from datetime import datetime
+
+today = datetime.today().strftime('%Y%m%d')
 
 f = open('downloads/response.json')
 response = json.load(f)
@@ -9,9 +12,9 @@ df = pd.DataFrame(response).explode('jobSearchs')
 df = pd.concat([df, df['jobSearchs'].apply(pd.Series)], axis=1)
 df.drop(columns=['jobSearchs'], inplace=True)
 df.dropna(inplace=True)
-df['info'] = df['info'].apply(lambda x: '\n'.join([i for i in x if not re.match('(Hide|.+recruiting|.+profile|.*alum|.+connections|.+applicants|.+ago|Within|Promoted)', i)]))
+df['info'] = df['info'].apply(lambda x: '\n'.join([i for i in x if not re.match('(Hide|.+recruiting|.+profile|.*alum|.+connection|.+applicants|.+ago|.+badge|Within|Promoted)', i)]))
 df = df.reset_index().drop(columns=['index'])
 df['date'] = pd.to_datetime(df['date']).dt.date
 df['link'] = df['link'].apply(lambda x: x.split('?')[0])
 df = df.drop_duplicates(['link'], keep="first")
-df.to_csv('downloads/data.csv', index=False)
+df.to_csv(f'downloads/job_searchs_{today}.csv', index=False)
